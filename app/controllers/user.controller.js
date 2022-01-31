5c7dfe5d6a05e8587743d9d647f6ed499a3592db
Bolaji Olayinka <bolajiolayinka980@gmail.com>
2022-01-31 11:13:17 +0100

    Sign in controller done

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -5,89 +5,68 @@ const shortid = require("shortid");
 const cloudinary = require("../utils/cloudinary");
 
 const generateJwtToken = (_id, role) => {
-  return jwt.sign({ _id, role },`${process.env.JWT_SECRET}`, {
+  return jwt.sign({ _id, role }, `${process.env.JWT_SECRET}`, {
     expiresIn: "1d",
   });
 };
 
 exports.signup = async (req, res) => {
- try{
-  const urls = [];
-   if(req.files){
-    const files = Object.values(req.files);
-    for (const file of files) {
-      const { path } = file[0];
-      const newPath = await cloudinary.uploader.upload(path, {
-        folder: "user",
-      });
-      urls.push(newPath);
+  try {
+    const urls = [];
+    if (req.files) {
+      const files = Object.values(req.files);
+      for (const file of files) {
+        const { path } = file[0];
+        const newPath = await cloudinary.uploader.upload(path, {
+          folder: "user",
+        });
+        urls.push(newPath);
+      }
     }
-   }
 
-  // res.send(req)
-  User.findOne({ email: req.body.email }).exec(async (error, user) => {
-    if (user)
-      return res.status(400).json({
-        error: "User already registered",
-      });
-     
-    const { email, password,user_type,contact_number,business_name  } = req.body;
-    const hash_password = await bcrypt.hash(password, 10);
-    const _user = new User({
-      email,
-      password,
-      user_type,
-      contact_number,
-      business_name,
-      hash_password,
-      licence_doc:urls[0] !==undefined ? urls[0].url : '',
-      profile_picture:urls[1] !==undefined ? urls[1].url : '',
-      username: shortid.generate(),
-    });
-     _user.save((error, user) => {
-      if (error) {
-        console.log(error)
+    // res.send(req)
+    User.findOne({ email: req.body.email }).exec(async (error, user) => {
+      if (user)
         return res.status(400).json({
-          message: "Something went wrong",
+          error: "User already registered",
         });
-      }
 
-      if (user) {
-        const token = generateJwtToken(user._id, user.role);
-        const { _id,email, role, business_name } = user;
-        return res.status(201).json({
-          token,
-          user: { _id, email, business_name, role },
-        });
-      }
+      const { email, password, user_type, contact_number, business_name } =
+        req.body;
+      const hash_password = await bcrypt.hash(password, 10);
+      const _user = new User({
+        email,
+        password,
+        user_type,
+        contact_number,
+        business_name,
+        hash_password,
+        licence_doc: urls[0] !== undefined ? urls[0].url : "",
+        profile_picture: urls[1] !== undefined ? urls[1].url : "",
+        username: shortid.generate(),
+      });
+      _user.save((error, user) => {
+        if (error) {
+          console.log(error);
+          return res.status(400).json({
+            message: "Something went wrong",
+          });
+        }
+
+        if (user) {
+          const token = generateJwtToken(user._id, user.role);
+          const { _id, email, role, business_name } = user;
+          return res.status(201).json({
+            token,
+            user: { _id, email, business_name, role },
+          });
+        }
+      });
     });
- })
-    
-    
-  
-  
-  
-
-    // _user.save((error, user) => {
-    //   if (error) {
-    //     return res.status(400).json({
-    //       message: "Something went wrong",
-    //     });
-    //   }
-
-    //   if (user) {
-    //     const token = generateJwtToken(user._id, user.role);
-    //     const { _id, firstName, lastName, email, role, fullName } = user;
-    //     return res.status(201).json({
-    //       token,
-    //       user: { _id, firstName, lastName, email, role, fullName },
-    //     });
-    //   }
-    // });
-  }catch(e){
-    console.log(e)
+  } catch (e) {
+    console.log(e);
   }
-}
+};
 
 exports.signin = (req, res) => {
   User.findOne({ email: req.body.email }).exec(async (error, user) => {
@@ -101,18 +80,22 @@ exports.signin = (req, res) => {
         //   { expiresIn: "1d" }
         // );
         const token = generateJwtToken(user._id, user.role);
-        const { _id, firstName, lastName, email, role, fullName } = user;
+        const { _id, email, role, business_name } = user;
         res.status(200).json({
           token,
-          user: { _id, firstName, lastName, email, role, fullName },
+          user: { _id, email, role, business_name },
         });
       } else {
-        return res.status(400).json({
+        return res.status(401).json({
           message: "Something went wrong",
         });
       }
     } else {
-      return res.status(400).json({ message: "Something went wrong" });
+      return res
+        .status(401)
+        .json({
+          message: "UnAuthorised Sign in, please check your login and password",
+        });
     }
   });
 };
