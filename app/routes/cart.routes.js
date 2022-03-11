674d0ecf2b6ed1a84d87b23ee4bbda50a558b3a4
Bolaji Olayinka <bolajiolayinka980@gmail.com>
2022-03-11 03:17:07 +0100

    changed cart routes

--- app/routes/cart.routes.js
+++ app/routes/cart.routes.js
@@ -2,12 +2,12 @@ module.exports = (app) => {
     const Cart = require('../controllers/cart.controller.js');
     const { requireSignin, userMiddleware } = require('../utils/middleware');
     // Add items to cart
-    app.post('/user/cart/addtocart',requireSignin,userMiddleware, Cart.addItemToCart);
+    app.post('/api/cart/addtocart',requireSignin,userMiddleware, Cart.addItemToCart);
 
     // Get Items in a cart
-    app.get('/user/cart/getcart',requireSignin,userMiddleware, Cart.getCartItems);
+    app.get('/api/cart/getcart',requireSignin,userMiddleware, Cart.getCartItems);
 
     // Delete an Item in Cart
-    app.delete('/user/cart/:id', Cart.removeCartItems);
+    app.delete('/api/cart/:id', Cart.removeCartItems);
 
 }
